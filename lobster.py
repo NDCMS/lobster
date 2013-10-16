23ad1a72f389f9f756f6bc54e2b8b2733e12bd9d
annawoodard <awoodard@nd.edu>
2013-10-16 11:14:28 -0400

    Adding YAML config file and argument parser

--- lobster.py
+++ lobster.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
-
 import os
-
+import yaml
 from lobster import das_interface, sandbox
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='A job submission tool for CMS')
+parser.add_argument('config_file_name', nargs='?', default='lobster.yaml', help='Configuration file to process.')
+args = parser.parse_args()
+
+with open(args.config_file_name) as config_file:
+    config = yaml.load(config_file)
 
 sandbox.package(os.environ['LOCALRT'], 'cmssw.tar.bz2')

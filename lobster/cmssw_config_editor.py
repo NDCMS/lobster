0942213899c4877d846b375df9cd0387979a6772
annawoodard <awoodard@nd.edu>
2013-10-31 15:27:10 -0400

    Fixing typo.

--- lobster/cmssw_config_editor.py
+++ lobster/cmssw_config_editor.py
@@ -8,7 +8,7 @@ def edit_IO_files(cmssw_config_filename, modified_config_filename, config_params
     (dataset_files, lumis) = config_params
     with open(modified_config_filename, 'a') as modified_config_file:
         fragment = ('import FWCore.ParameterSet.Config as cms'
-                    '\nprocess.source.fileNames = cms.untracked.vstring({input_files}))'
+                    '\nprocess.source.fileNames = cms.untracked.vstring({input_files})'
                     '\nprocess.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))'
                     '\nprocess.source.lumisToProcess = cms.untracked.VLuminosityBlockRange({lumis})')
         modified_config_file.write(fragment.format(input_files=repr(dataset_files), lumis=lumis))

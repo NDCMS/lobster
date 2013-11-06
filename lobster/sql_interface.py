c512279861f2f9632531d080688e0e2421d19b22
annawoodard <awoodard@nd.edu>
2013-11-06 11:02:26 -0500

    Adding job id to table.

--- lobster/sql_interface.py
+++ lobster/sql_interface.py
@@ -7,11 +7,13 @@ class SQLInterface:
         self.config = config
         self.db_path = os.path.join(config['workdir'], "lobster.db")
         self.db = sqlite3.connect(self.db_path) #to do: add handling to check for lost jobs from previous execution, set status to failed
-        self.db.execute("create table if not exists jobits(dataset, input_file, run, lumi, status, num_attempts, host, exit_code, run_time, startup_time)")
+        self.db.execute("create table if not exists jobits(job_id, dataset, input_file, run, lumi, status, num_attempts, host, exit_code, run_time, startup_time)")
+        self.job_id_counter = 0
+        self.db.commit()
 
     def disconnect(self):
         self.db.close()
-        
+
     def register_jobits(self, das):
         datasets = [x['dataset'] for x in self.config['tasks']]
         for dataset in datasets:
@@ -28,12 +30,13 @@ class SQLInterface:
         self.db.commit()
 
     def pop_jobits(self, size, old_status, new_status):
+        self.job_id_counter += 1
         input_files = []
         lumis = []
         for input_file, run, lumi in self.db.execute("select input_file, run, lumi from jobits where status=? limit ?", (old_status, size)):
             input_files.append(input_file)
             lumis.append((run, lumi))
-            self.db.execute("update jobits set status=? where run=? and lumi=?", (new_status, run, lumi))
+            self.db.execute("update jobits set status=?, job_id=? where run=? and lumi=?", (new_status, self.job_id_counter, run, lumi))
 
         self.db.commit()
 

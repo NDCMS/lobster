fe314468c4a8c02650b3b983a239acd06bfc003f
Matthias Wolf <matthias@sushinara.net>
2013-11-08 15:51:01 -0500

    Improve config file handling on the job.

--- lobster/cmssw/data/job.py
+++ lobster/cmssw/data/job.py
@@ -4,21 +4,29 @@ import base64
 import json
 import os
 import pickle
+import shutil
 import subprocess
 import sys
 
+fragment = """import FWCore.ParameterSet.Config as cms
+process.source.fileNames = cms.untracked.vstring({input_files})
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange({lumis})"""
+
 def edit_process_source(cmssw_config_file, config_params):
     (dataset_files, lumis) = config_params
-    config = open(cmssw_config_file, 'a')
     with open(cmssw_config_file, 'a') as config:
-        fragment = ('import FWCore.ParameterSet.Config as cms'
-                    '\nprocess.source.fileNames = cms.untracked.vstring({input_files})'
-                    '\nprocess.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))'
-                    '\nprocess.source.lumisToProcess = cms.untracked.VLuminosityBlockRange({lumis})')
-        config.write(fragment.format(input_files=repr([str(f) for f in dataset_files]), lumis=[str(l) for l in lumis]))
+        frag = fragment.format(input_files=repr([str(f) for f in dataset_files]), lumis=[str(l) for l in lumis])
+        print "--- config file fragment:"
+        print frag
+        print "---"
+        config.write(frag)
 
-(configfile, inputs) = sys.argv[1:3]
+(config, inputs) = sys.argv[1:3]
 args = sys.argv[3:]
+
+configfile = config.replace(".py", "_mod.py")
+shutil.copy2(config, configfile)
 
 for d in os.listdir('.'):
     if d.startswith('CMSSW'):

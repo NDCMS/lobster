862ad108f7a0cde0eeded35275a3f9dc0791a0cf
Matthias Wolf <matthias@sushinara.net>
2014-02-02 12:10:48 -0500

    Switch info plots to sqlite and numpy.
    Remove now obsolete parsing library (partially already integrated into cmssw.data.job).

--- scripts/plot_stats.py
+++ scripts/plot_stats.py
@@ -4,10 +4,12 @@ from argparse import ArgumentParser
 from collections import defaultdict
 import glob
 import os
+import sqlite3
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-from lobster.utils import parse, web
+from lobster.utils import web
 
 def diff_times(start_times, end_times):
     time_diff = lambda (a, b): (a - b).seconds / 60
@@ -68,6 +70,7 @@ def make_scatter(x, x_label, y, y_label, t, name, dir):
     save_and_close(dir, name)
 
 def save_and_close(dir, name):
+    print "Saving", name
     plt.savefig(os.path.join(dir, '%s.png' % name))
     plt.savefig(os.path.join(dir, '%s.pdf' % name))
 
@@ -80,49 +83,104 @@ if __name__ == '__main__':
     parser.add_argument('outdir')
     args = parser.parse_args()
 
+    db = sqlite3.connect(os.path.join(args.directory, 'lobster.db'))
+
     stats = {}
     top_dir = args.outdir
     # top_dir = os.path.join('/afs/crc.nd.edu/user/a/awoodard/www/lobster/', '29-01-2014')
     # top_dir = os.path.join('/afs/crc.nd.edu/user/a/awoodard/www/lobster/', datetime.today().strftime('%d-%m-%Y'))
 
-    for label, num_bins in [('TTJets', 15), ('ttW', 7)]:
-        stats[label] = get_stats(args.directory, label)
+    label2id = {}
+    id2label = {}
+
+    for dset_label, dset_id in db.execute('select label, id from datasets'):
+        label2id[dset_label] = dset_id
+        id2label[dset_id] = dset_label
+
+    for (label, id) in label2id.items():
+        num_bins = 30
+        # stats[label] = get_stats(args.directory, label)
         save_dir = os.path.join(top_dir, label)
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
 
-        make_histo([(diff_times(stats[label]['ready'], stats[label]['start']), '')], num_bins,
+        import time
+        print "Processing", label, id
+        # FIXME convert dataset to id again!
+        vals = np.array(db.execute('select * from jobs where status=2 and dataset=?', (label,)).fetchall(),
+                dtype=[
+                    ('id', 'i4'),
+                    ('host', 'a50'),
+                    ('dataset', 'a50'),
+                    ('status', 'i4'),
+                    ('exit_code', 'i4'),
+                    ('missed_lumis', 'i4'),
+                    ('t_submit', 'i4'),
+                    ('t_send_start', 'i4'),
+                    ('t_send_end', 'i4'),
+                    ('t_wrapper_start', 'i4'),
+                    ('t_wrapper_ready', 'i4'),
+                    ('t_file_req', 'i4'),
+                    ('t_file_open', 'i4'),
+                    ('t_first_ev', 'i4'),
+                    ('t_wrapper_end', 'i4'),
+                    ('t_recv_start', 'i4'),
+                    ('t_recv_end', 'i4'),
+                    ('t_retrieved', 'i4'),
+                    ])
+        # for i in db.execute('select * from jobs where status=2 and dataset=?', (id,)).fetchall():
+            # print i
+        if len(vals) == 0:
+            continue
+
+        make_histo([((vals['t_wrapper_ready'] - vals['t_wrapper_start']) / 60., '')], num_bins,
                    'time from wrapper start to ready (%s)' % label,
                    'time (m)', 'jobits', 'start_to_ready', save_dir)
-        make_histo([(diff_times(stats[label]['done'], stats[label]['first']), '')], num_bins,
+        make_histo([((vals['t_wrapper_end'] - vals['t_first_ev']) / 60., '')], num_bins,
                    'time from first event processed to finished (%s)' % label,
                    'time (m)', 'jobits', 'first_to_done', save_dir)
-        make_histo([(diff_times(stats[label]['fopen'], stats[label]['start']), '')], num_bins,
-                   'time from wrapper start to file open (%s)' % label,
-                   'time (m)', 'jobits', 'start_to_fopen', save_dir)
-        make_histo([(diff_times(stats[label]['first'], stats[label]['start']), '')], num_bins,
-                   'time from wrapper start to first event processed (%s)' % label,
-                   'time (m)', 'jobits', 'start_to_first', save_dir)
-        make_histo([(diff_times(stats[label]['done'], stats[label]['start']), '')], num_bins,
+        make_histo([((vals['t_recv_end'] - vals['t_wrapper_end']) / 60., '')], num_bins,
+                   'time from end of wrapper to end of transfer (%s)' % label,
+                   'time (m)', 'jobits', 'end_to_over', save_dir)
+        make_histo([((vals['t_recv_end'] - vals['t_recv_start']) / 60., '')], num_bins,
+                   'duration of final transfer (%s)' % label,
+                   'time (m)', 'jobits', 'transfer', save_dir)
+        # make_histo([(diff_times(stats[label]['fopen'], stats[label]['start']), '')], num_bins,
+                   # 'time from wrapper start to file open (%s)' % label,
+                   # 'time (m)', 'jobits', 'start_to_fopen', save_dir)
+        # make_histo([(diff_times(stats[label]['first'], stats[label]['start']), '')], num_bins,
+                   # 'time from wrapper start to first event processed (%s)' % label,
+                   # 'time (m)', 'jobits', 'start_to_first', save_dir)
+        make_histo([((vals['t_wrapper_end'] - vals['t_wrapper_start']) / 60., '')], num_bins,
                    'time from wrapper start to processing finished (%s)' % label,
                    'time (m)', 'jobits', 'start_to_done', save_dir)
-        make_histo([(stats[label]['changed servers'], '')], num_bins,
-                   'number of XrootD server changes (%s)' % label,
-                   'changed_servers', 'jobits', 'server_changes', save_dir)
-        make_scatter(diff_times(stats[label]['done'], stats[label]['start']), 'time to finish processing',
-                     stats[label]['changed servers'], 'number of XrootD server changes', label, 'changed_servers_vs_time', save_dir)
+        # make_histo([(stats[label]['changed servers'], '')], num_bins,
+                   # 'number of XrootD server changes (%s)' % label,
+                   # 'changed_servers', 'jobits', 'server_changes', save_dir)
+        # make_scatter(diff_times(stats[label]['done'], stats[label]['start']), 'time to finish processing',
+                     # stats[label]['changed servers'], 'number of XrootD server changes', label, 'changed_servers_vs_time', save_dir)
+        # make_scatter((vals['t_wrapper_end'] - vals['t_first_ev']) / 60., 'processing time',
+                # np.char.rstrip(vals['host'], '0123456789'), 'host (cluster)', label, 'host_processing_times', save_dir)
 
-    make_histo([(diff_times(stats['TTJets']['done'], stats['TTJets']['first']), 'TTJets'),
-                (diff_times(stats['ttW']['done'], stats['ttW']['first']), 'ttW')], 10,
-                 'time from first event processed to finished', 'time (m)', 'jobits', 'first_to_done_overlay', top_dir)
+    # make_histo([(diff_times(stats['TTJets']['done'], stats['TTJets']['first']), 'TTJets'),
+                # (diff_times(stats['ttW']['done'], stats['ttW']['first']), 'ttW')], 10,
+                 # 'time from first event processed to finished', 'time (m)', 'jobits', 'first_to_done_overlay', top_dir)
 
-    wq_stats = parse.get_wq_stats(args.wq_logfile)
-    run_times = [(int(x) - int(y)) / (60 * 1e6) for x, y in zip(wq_stats['timestamp'], wq_stats['start_time'])]
-    make_plot([(run_times, wq_stats['workers_active'], 'workers active'),
-               (run_times, wq_stats['workers_ready'], 'workers ready'),
-               (run_times, wq_stats['tasks_running'], 'tasks running'),
-               (run_times, wq_stats['total_workers_connected'], 'total workers connected'),
+    print "Reading WQ log"
+    with open(args.wq_logfile) as f:
+        headers = dict(map(lambda (a, b): (b, a), enumerate(f.readline()[1:].split())))
+    wq_stats = np.loadtxt(args.wq_logfile)
+    # subtract start time, convert to minutes
+    wq_stats[:,0] = (wq_stats[:,0] - wq_stats[0,1]) / 60e6
+    # wq_stats = parse.get_wq_stats(args.wq_logfile)
+    print "Done reading WQ log"
+    run_times = map(int, wq_stats[:,0])
+    make_plot([(run_times, wq_stats[:,headers['workers_active']], 'workers active'),
+               (run_times, wq_stats[:,headers['workers_ready']], 'workers ready'),
+               (run_times, wq_stats[:,headers['tasks_running']], 'tasks running'),
+               (run_times, wq_stats[:,headers['total_workers_connected']], 'total workers connected'),
                # (run_times, wq_stats['total_workers_removed'], 'workers removed'),
-               (run_times, wq_stats['total_tasks_complete'], 'total tasks complete')], 'time', 'workers active', '', 'workers_active', top_dir)
+               # (run_times, wq_stats['total_tasks_complete'], 'total tasks complete')
+               ], 'time', 'workers active', '', 'workers_active', top_dir)
 
     web.update_indexes(args.outdir)

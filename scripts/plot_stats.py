467f1fc1b58cf55b158d720a20c9ad50db0f65ad
Matthias Wolf <matthias@sushinara.net>
2014-02-19 16:14:15 -0500

    Rework plotting.
    This will now create one single overview page...

--- scripts/plot_stats.py
+++ scripts/plot_stats.py
@@ -3,13 +3,31 @@
 from argparse import ArgumentParser
 from collections import defaultdict
 import glob
+import math
 import os
 import sqlite3
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from lobster.utils import web
+matplotlib.rc('axes', labelsize='large')
+matplotlib.rc('figure', figsize=(8, 1.5))
+matplotlib.rc('figure.subplot', left=0.09, right=0.92, bottom=0.22)
+matplotlib.rc('font', size=7)
+
+class SmartList(list):
+    """Stupid extended list."""
+    def __init__(self, *args, **kwargs):
+        list.__init__(self, *args, **kwargs)
+    def __add__(self, other):
+        return super(SmartList, self).__add__([other])
+    def __iadd__(self, other):
+        return super(SmartList, self).__iadd__([other])
+
+def html_tag(tag, *args, **kwargs):
+    attr = " ".join(['{0}="{1}"'.format(a, b.replace('"', r'\"')) for a, b in kwargs.items()])
+    return '<{0}>\n{1}\n</{2}>\n'.format(" ".join([tag, attr]), "\n".join(args), tag)
 
 def diff_times(start_times, end_times):
     time_diff = lambda (a, b): (a - b).seconds / 60
@@ -32,40 +50,55 @@ def get_stats(dir, label):
 
     return s
 
-def make_histo(ranges, num_bins, t, xlabel, ylabel, filename, dir):
-    histtype = 'bar'
-    linewidth = 1
-    if len(ranges) > 1:
-        histtype = 'step'
-        linewidth = 2
+def make_histo(a, num_bins, xlabel, ylabel, filename, dir, **kwargs):
+    # fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True)
+    if 'log' in kwargs:
+        if kwargs['log']:
+            plt.yscale('log')
+        del kwargs['log']
 
-    fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True)
-    ax1.set_yscale('log')
+    plt.hist(a, bins=num_bins, histtype='barstacked', **kwargs)
 
-    for r, l in ranges:
-        ax0.hist(r, bins=num_bins, label=l, histtype=histtype)
-        ax1.hist(r, bins=num_bins, label=l, histtype=histtype)
+    # ax0.set_title(t)
 
-    ax0.set_title(t)
+    # ax0.set_ylabel(ylabel)
+    # ax1.set_xlabel(xlabel)
+    # ax1.set_ylabel(ylabel)
 
-    ax0.set_ylabel(ylabel)
-    ax1.set_xlabel(xlabel)
-    ax1.set_ylabel(ylabel)
+    # plt.set_title(t)
+    # plt.set_xlabel(xlabel)
+    # plt.set_ylabel(ylabel)
 
-    if len(ranges) > 1:
+    # plt.title(t)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    if 'label' in kwargs:
         plt.legend()
 
-    save_and_close(dir, filename)
+    return save_and_close(dir, filename)
 
-def make_plot(tuples, x_label, y_label, t, name, dir, fun=plt.plot):
-    for x, y, l in tuples:
-        fun(x, y, label=l)
-    plt.title(t)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.legend(loc='lower right')
+def make_plot(tuples, x_label, y_label, name, dir, fun=matplotlib.axes.Axes.plot, y_label2=None):
+    fig, ax1 = plt.subplots()
 
-    save_and_close(dir, name)
+    plots1 = tuples[0] if y_label2 else tuples
+    for x, y, l in plots1:
+        fun(ax1, x, y, label=l)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y_label)
+    ax1.legend(loc='upper left')
+
+    if y_label2:
+        ax2 = ax1.twinx()
+        for x, y, l in tuples[1]:
+            fun(ax2, x, y, ':', label=l)
+        ax2.set_ylabel(y_label2)
+        ax2.legend(loc='upper right')
+
+    # plt.legend()
+    num = len(tuples[0]) + len(tuples[1]) if y_label2 else len(tuples)
+
+    return save_and_close(dir, name)
 
 def make_scatter(x, x_label, y, y_label, t, name, dir):
     plt.scatter(x, y)
@@ -73,7 +106,7 @@ def make_scatter(x, x_label, y, y_label, t, name, dir):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
 
-    save_and_close(dir, name)
+    return save_and_close(dir, name)
 
 def reduce(a, idx, interval):
     quant = a[:,idx]
@@ -88,17 +121,32 @@ def reduce(a, idx, interval):
 
     return a[select]
 
+def split_by_column(a, col, key=lambda x: x):
+    """Split an array into multiple ones, based on unique values in the named
+    column `col`.
+    """
+    vals = np.unique(a[col])
+    ret = []
+    for v in vals:
+        sel = a[col] == v
+        ret.append((key(v), a[sel]))
+    return ret
+
 def save_and_close(dir, name):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
     print "Saving", name
+    # plt.gcf().set_size_inches(6, 1.5)
     plt.savefig(os.path.join(dir, '%s.png' % name))
     plt.savefig(os.path.join(dir, '%s.pdf' % name))
 
     plt.close()
 
+    return html_tag("img", src='{0}.png'.format(name))
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='make histos')
     parser.add_argument('directory')
-    parser.add_argument('wq_logfile')
     parser.add_argument('outdir')
     args = parser.parse_args()
 
@@ -109,6 +157,45 @@ if __name__ == '__main__':
     # top_dir = os.path.join('/afs/crc.nd.edu/user/a/awoodard/www/lobster/', '29-01-2014')
     # top_dir = os.path.join('/afs/crc.nd.edu/user/a/awoodard/www/lobster/', datetime.today().strftime('%d-%m-%Y'))
 
+    failed_jobs = np.array(db.execute("""select
+        id,
+        host,
+        dataset,
+        exit_code,
+        time_submit,
+        time_retrieved
+        from jobs where status=3""").fetchall(),
+            dtype=[
+                ('id', 'i4'),
+                ('host', 'a50'),
+                ('dataset', 'i4'),
+                ('exit_code', 'i4'),
+                ('t_submit', 'i4'),
+                ('t_retrieved', 'i4')
+                ])
+
+    success_jobs = np.array(db.execute('select * from jobs where status=2').fetchall(),
+            dtype=[
+                ('id', 'i4'),
+                ('host', 'a50'),
+                ('dataset', 'i4'),
+                ('status', 'i4'),
+                ('exit_code', 'i4'),
+                ('missed_lumis', 'i4'),
+                ('t_submit', 'i4'),
+                ('t_send_start', 'i4'),
+                ('t_send_end', 'i4'),
+                ('t_wrapper_start', 'i4'),
+                ('t_wrapper_ready', 'i4'),
+                ('t_file_req', 'i4'),
+                ('t_file_open', 'i4'),
+                ('t_first_ev', 'i4'),
+                ('t_wrapper_end', 'i4'),
+                ('t_recv_start', 'i4'),
+                ('t_recv_end', 'i4'),
+                ('t_retrieved', 'i4')
+                ])
+
     label2id = {}
     id2label = {}
 
@@ -116,108 +203,74 @@ if __name__ == '__main__':
         label2id[dset_label] = dset_id
         id2label[dset_id] = dset_label
 
-    for (label, id) in label2id.items():
-        num_bins = 30
-        # stats[label] = get_stats(args.directory, label)
-        save_dir = os.path.join(top_dir, label)
-        if not os.path.isdir(save_dir):
-            os.makedirs(save_dir)
+    dset_values = split_by_column(success_jobs, 'dataset', key=lambda x: id2label[x])
 
-        import time
-        print "Processing", label, id
-        # FIXME convert dataset to id again!
-        vals = np.array(db.execute('select * from jobs where status=2 and dataset=?', (label,)).fetchall(),
-                dtype=[
-                    ('id', 'i4'),
-                    ('host', 'a50'),
-                    ('dataset', 'a50'),
-                    ('status', 'i4'),
-                    ('exit_code', 'i4'),
-                    ('missed_lumis', 'i4'),
-                    ('t_submit', 'i4'),
-                    ('t_send_start', 'i4'),
-                    ('t_send_end', 'i4'),
-                    ('t_wrapper_start', 'i4'),
-                    ('t_wrapper_ready', 'i4'),
-                    ('t_file_req', 'i4'),
-                    ('t_file_open', 'i4'),
-                    ('t_first_ev', 'i4'),
-                    ('t_wrapper_end', 'i4'),
-                    ('t_recv_start', 'i4'),
-                    ('t_recv_end', 'i4'),
-                    ('t_retrieved', 'i4'),
-                    ])
-        # for i in db.execute('select * from jobs where status=2 and dataset=?', (id,)).fetchall():
-            # print i
-        if len(vals) == 0:
-            continue
+    num_bins = 30
+    total_times = [(vs[1]['t_wrapper_end'] - vs[1]['t_wrapper_start']) / 60. for vs in dset_values]
+    processing_times = [(vs[1]['t_wrapper_end'] - vs[1]['t_first_ev']) / 60. for vs in dset_values]
+    overhead_times = [(vs[1]['t_first_ev'] - vs[1]['t_wrapper_start']) / 60. for vs in dset_values]
+    stageout_times = [(vs[1]['t_retrieved'] - vs[1]['t_wrapper_end']) / 60. for vs in dset_values]
 
-        make_histo([((vals['t_wrapper_ready'] - vals['t_wrapper_start']) / 60., '')], num_bins,
-                   'time from wrapper start to ready (%s)' % label,
-                   'time (m)', 'jobits', 'start_to_ready', save_dir)
-        make_histo([((vals['t_wrapper_end'] - vals['t_first_ev']) / 60., '')], num_bins,
-                   'time from first event processed to finished (%s)' % label,
-                   'time (m)', 'jobits', 'first_to_done', save_dir)
-        make_histo([((vals['t_recv_end'] - vals['t_wrapper_end']) / 60., '')], num_bins,
-                   'time from end of wrapper to end of transfer (%s)' % label,
-                   'time (m)', 'jobits', 'end_to_over', save_dir)
-        make_histo([((vals['t_recv_end'] - vals['t_recv_start']) / 60., '')], num_bins,
-                   'duration of final transfer (%s)' % label,
-                   'time (m)', 'jobits', 'transfer', save_dir)
-        # make_histo([(diff_times(stats[label]['fopen'], stats[label]['start']), '')], num_bins,
-                   # 'time from wrapper start to file open (%s)' % label,
-                   # 'time (m)', 'jobits', 'start_to_fopen', save_dir)
-        # make_histo([(diff_times(stats[label]['first'], stats[label]['start']), '')], num_bins,
-                   # 'time from wrapper start to first event processed (%s)' % label,
-                   # 'time (m)', 'jobits', 'start_to_first', save_dir)
-        make_histo([((vals['t_wrapper_end'] - vals['t_wrapper_start']) / 60., '')], num_bins,
-                   'time from wrapper start to processing finished (%s)' % label,
-                   'time (m)', 'jobits', 'start_to_done', save_dir)
-        # make_histo([(stats[label]['changed servers'], '')], num_bins,
-                   # 'number of XrootD server changes (%s)' % label,
-                   # 'changed_servers', 'jobits', 'server_changes', save_dir)
-        # make_scatter(diff_times(stats[label]['done'], stats[label]['start']), 'time to finish processing',
-                     # stats[label]['changed servers'], 'number of XrootD server changes', label, 'changed_servers_vs_time', save_dir)
-        # make_scatter((vals['t_wrapper_end'] - vals['t_first_ev']) / 60., 'processing time',
-                # np.char.rstrip(vals['host'], '0123456789'), 'host (cluster)', label, 'host_processing_times', save_dir)
+    jtags = SmartList()
+    wtags = SmartList()
 
-    # make_histo([(diff_times(stats['TTJets']['done'], stats['TTJets']['first']), 'TTJets'),
-                # (diff_times(stats['ttW']['done'], stats['ttW']['first']), 'ttW')], 10,
-                 # 'time from first event processed to finished', 'time (m)', 'jobits', 'first_to_done_overlay', top_dir)
+    jtags += make_histo(total_times, num_bins, 'Runtime (m)', 'Jobs', 'run_time', top_dir, label=[vs[0] for vs in dset_values])
+    jtags += make_histo(processing_times, num_bins, 'Pure processing time (m)', 'Jobs', 'processing_time', top_dir, label=[vs[0] for vs in dset_values])
+    jtags += make_histo(overhead_times, num_bins, 'Overhead time (m)', 'Jobs', 'overhead_time', top_dir, label=[vs[0] for vs in dset_values])
+    jtags += make_histo(stageout_times, num_bins, 'Stage-out time (m)', 'Jobs', 'stageout_time', top_dir, label=[vs[0] for vs in dset_values])
+
+    # hosts = vals['host']
+    # host_clusters = np.char.rstrip(np.char.replace(vals['host'], '.crc.nd.edu', ''), '0123456789-')
+
+    # web.update_indexes(args.outdir)
+    # raise
 
     print "Reading WQ log"
-    with open(args.wq_logfile) as f:
+    with open(os.path.join(args.directory, 'work_queue.log')) as f:
         headers = dict(map(lambda (a, b): (b, a), enumerate(f.readline()[1:].split())))
-    wq_stats_raw = np.loadtxt(args.wq_logfile)
+    wq_stats_raw = np.loadtxt(os.path.join(args.directory, 'work_queue.log'))
+    orig_times = wq_stats_raw[:,0].copy()
     # subtract start time, convert to minutes
-    wq_stats_raw[:,0] = (wq_stats_raw[:,0] - wq_stats_raw[0,1]) / 60e6
+    start_time = wq_stats_raw[0,0]
+    end_time = wq_stats_raw[-1,0]
+    wq_stats_raw[:,0] = (wq_stats_raw[:,0] - start_time) / 60e6
     runtimes = wq_stats_raw[:,0]
+    print "First iteration..."
 
     bins = xrange(0, runtimes[-1] + 5, 5)
-    make_histo([(runtimes, '')], bins,
-            'Activity in 5 min time slices',
-            'time(m)', 'activity', 'activity', top_dir)
+    wtags += make_histo([runtimes], bins, 'Time (m)', 'Activity', 'activity', top_dir, log=True)
+
+    transferred = (wq_stats_raw[:,headers['total_bytes_received']] - np.roll(wq_stats_raw[:,headers['total_bytes_received']], 1, 0)) / 1024**3
+    transferred[0] = 0
+    bins = xrange(0, runtimes[-1] + 60, 60)
+    wtags += make_histo([runtimes], bins, 'Time (m)', 'Output (GB/h)', 'rate', top_dir, weights=[transferred])
+
+    # gap_indices = np.logical_or((np.roll(runtimes, -1) - runtimes) > 5, (runtimes - np.roll(runtimes, 1)) > 5)
+    # gap_indices = np.logical_or(gap_indices, np.logical_or(np.roll(gap_indices, -1), np.roll(gap_indices, 1)))
+    # print len(wq_stats_raw[gap_indices])
+    # np.savetxt('gaps.txt', orig_times[gap_indices], '%30.1f')
+    # sys.exit()
 
     print "Reducing WQ log"
     wq_stats = reduce(wq_stats_raw, 0, 5.)
     runtimes = wq_stats[:,0]
 
-    make_plot([(runtimes, wq_stats[:,headers['workers_active']], 'workers active'),
-               (runtimes, wq_stats[:,headers['workers_ready']], 'workers ready'),
-               (runtimes, wq_stats[:,headers['tasks_running']], 'tasks running'),
-               (runtimes, wq_stats[:,headers['total_workers_connected']], 'total workers connected'),
-               # (runtimes, wq_stats['total_workers_removed'], 'workers removed'),
-               # (runtimes, wq_stats['total_tasks_complete'], 'total tasks complete')
-               ], 'time', 'workers active', '', 'workers_active', top_dir)
+    wtags += make_plot(([(runtimes, wq_stats[:,headers['workers_busy']], 'busy'),
+               (runtimes, wq_stats[:,headers['workers_idle']], 'idle'),
+               (runtimes, wq_stats[:,headers['total_workers_connected']], 'connected')],
+               [(runtimes, wq_stats[:,headers['tasks_running']], 'running')]),
+               'Time (m)', 'Workers' , 'workers_active', top_dir, y_label2='Tasks')
 
-    print "Reducing WQ log again"
-    wq_stats = reduce(wq_stats_raw, 0, 60.)
-    runtimes = wq_stats[:,0]
+    bins = xrange(0, runtimes[-1] + 5, 5)
+    success_times = (success_jobs['t_retrieved'] - start_time / 1e6) / 60
+    failed_times = (failed_jobs['t_retrieved'] - start_time / 1e6) / 60
+    wtags += make_histo([success_times, failed_times], bins, 'Time (m)', 'Jobs', 'jobs', top_dir, label=['succesful', 'failed'], color=['green', 'red'])
 
-    diff = wq_stats - np.roll(wq_stats, -1, axis=0)
-    rate = np.roll(diff[:,headers['total_bytes_received']] / diff[:,0] * 60. / 1024**3, -1)
-    select = rate >= 0
-    make_plot([(runtimes[select], rate[select], 'rate')],
-            'time', 'GB/h', '', 'rate', top_dir, fun=plt.step)
-
-    web.update_indexes(args.outdir)
+    with open(os.path.join(top_dir, 'index.html'), 'w') as f:
+        body = html_tag("div",
+                *([html_tag("h2", "Job Statistics")] +
+                    map(lambda t: html_tag("div", t, style="clear: both;"), jtags) +
+                    [html_tag("h2", "Lobster Statistics")] +
+                    map(lambda t: html_tag("div", t, style="clear: both;"), wtags)),
+                style="margin: 1em auto; display: block; width: auto; text-align: center;")
+        f.write(body)

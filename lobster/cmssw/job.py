efbe93b4d43ef95c6bba26af80546641dff217ea
Matthias Wolf <matthias@sushinara.net>
2014-05-12 15:23:02 -0400

    Saveguard against files that appear in the to process list, but not in the report.
    We should log theses events...

--- lobster/cmssw/job.py
+++ lobster/cmssw/job.py
@@ -80,7 +80,7 @@ class JobHandler(object):
         for (id, file) in self.__files:
             file_lumis = [tpl for tpl in self.__lumis if tpl[1] == id]
 
-            skipped = file in files_skipped
+            skipped = file in files_skipped or file not in files_info
             read = 0 if skipped or failed else files_info[file][0]
 
             if not self.__file_based:

NDCMS/lobster

commit d532e5a8bb7167e0cc9d31f882972631fd4aaf8c

Turn on cmssw wantSummary by default (but leave it possible to override in the lobster config)

lobster/cmssw/data/job.py
@@ -18,13 +18,15 @@ from ProdCommon.FwkJobRep.ReportParser import readJobReport
 fragment = """import FWCore.ParameterSet.Config as cms
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32({events}))"""
 
-def edit_process_source(cmssw_config_file, files, lumis, events=-1):
+def edit_process_source(cmssw_config_file, files, lumis, want_summary, events=-1):
     with open(cmssw_config_file, 'a') as config:
         frag = fragment.format(events=events)
         if any([f for f in files]):
             frag += "\nprocess.source.fileNames = cms.untracked.vstring({input_files})".format(input_files=repr([str(f) for f in files]))
         if lumis:
             frag += "\nprocess.source.lumisToProcess = cms.untracked.VLuminosityBlockRange({lumis})".format(lumis=[str(l) for l in lumis.getVLuminosityBlockRange()])
+        if want_summary:
+            frag += "\nprocess.options.wantSummary = cms.untracked.bool(True)"
         print "--- config file fragment:"
         print frag
         print "---"
@@ -79,7 +81,7 @@ def extract_cmssw_times(log_filename, default=None):
 
 (config, data) = sys.argv[1:]
 with open(data, 'rb') as f:
-    (args, files, lumis, stageout, taskid, monitorid, syncid) = pickle.load(f)
+    (args, files, lumis, stageout, taskid, monitorid, syncid, want_summary) = pickle.load(f)
 
 apmonSend(taskid, monitorid, {
             'ExeStart': 'cmsRun',

lobster/cmssw/job.py
@@ -271,8 +271,9 @@ class JobProvider(job.JobProvider):
             else:
                 outputs.extend([(os.path.join(jdir, f), f) for f in ['report.xml.gz', 'cmssw.log.gz', 'report.pkl']])
 
+                sum = self.config.get('cmssw summary', True)
                 with open(os.path.join(jdir, 'parameters.pkl'), 'wb') as f:
-                    pickle.dump((args, files, lumis, stageout, self.taskid, monitorid, syncid), f, pickle.HIGHEST_PROTOCOL)
+                    pickle.dump((args, files, lumis, stageout, self.taskid, monitorid, syncid, sum), f, pickle.HIGHEST_PROTOCOL)
                 inputs.append((os.path.join(jdir, 'parameters.pkl'), 'parameters.pkl'))
 
                 cmd = 'sh wrapper.sh python job.py {0} parameters.pkl'.format(cms_config)

commit 4b21d7038750d68d9a4284c0f82a1fb7a4a74912

Fixing an error I introduced in d532e5a8bb7167e0cc9d31f882972631fd4aaf8c.

lobster/cmssw/data/job.py
@@ -100,7 +100,7 @@ shutil.copy2(config, configfile)
 env = os.environ
 env['X509_USER_PROXY'] = 'proxy'
 
-edit_process_source(configfile, files, lumis)
+edit_process_source(configfile, files, lumis, want_summary)
 
 # exit_code = subprocess.call('python "{0}" {1}'.format(configfile, ' '.join(map(repr, args))), shell=True, env=env)
 exit_code = subprocess.call('cmsRun -j report.xml "{0}" {1} > cmssw.log 2>&1'.format(configfile, ' '.join(map(repr, args))), shell=True, env=env)
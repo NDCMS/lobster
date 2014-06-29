NDCMS/lobster

commit adc8368116204c8ea7a6b65abaa26666e88a55d6

Don't pop published jobits.

lobster/cmssw/jobit.py
@@ -270,7 +270,7 @@ class JobitStore:
                 for (ls_id, ls_file, ls_run, ls_lumi) in self.db.execute("""
                         select id, file, run, lumi
                         from jobits_{0}
-                        where run=? and lumi=? and status not in (1, 2)""".format(dataset), (run, lumi)):
+                        where run=? and lumi=? and status not in (1, 2, 3)""".format(dataset), (run, lumi)):
                     lumis.add((ls_id, ls_file, ls_run, ls_lumi))
                     files.add(ls_file)
             else:

commit ff94fd48536a0df0f87232d6059ef5ed3fe3f8a6

Fixing mistake I introduced in adc8368116204c8ea7a6b65abaa26666e88a55d6.

lobster/cmssw/jobit.py
@@ -270,7 +270,7 @@ class JobitStore:
                 for (ls_id, ls_file, ls_run, ls_lumi) in self.db.execute("""
                         select id, file, run, lumi
                         from jobits_{0}
-                        where run=? and lumi=? and status not in (1, 2, 3)""".format(dataset), (run, lumi)):
+                        where run=? and lumi=? and status not in (1, 2, 6)""".format(dataset), (run, lumi)):
                     lumis.add((ls_id, ls_file, ls_run, ls_lumi))
                     files.add(ls_file)
             else:
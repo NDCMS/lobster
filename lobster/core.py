a52062456cc9edb334abb5832fcc346cf9149efe
Matthias Wolf <matthias@sushinara.net>
2014-08-22 10:46:54 -0400

    Tune the wq scheduler.

--- lobster/core.py
+++ lobster/core.py
@@ -149,6 +149,7 @@ def run(args):
         queue.specify_keepalive_timeout(300)
         # queue.tune("short-timeout", 600)
         queue.tune("transfer-outlier-factor", 4)
+        queue.specify_algorithm(wq.WORK_QUEUE_SCHEDULE_RAND)
 
         logging.info("starting queue as {0}".format(queue.name))
         logging.info("submit workers with: condor_submit_workers -M {0} <num>".format(queue.name))

1c7cf1dbdf826f64864335761a0205383f8940b1
Anna Elizabeth Woodard <awoodard@nd.edu>
2015-08-07 03:48:14 -0400

    Only start a process for auto logging if it is enabled. Fixes #171.

--- lobster/cmssw/actions.py
+++ lobster/cmssw/actions.py
@@ -5,28 +5,34 @@ from lobster.cmssw.plotting import Plotter
 
 logger = multiprocessing.get_logger()
 
-class DummyPlotter(object):
-    def make_plots(*args, **kwargs):
+class DummyQueue(object):
+    def start(*args):
         pass
+
+    def put(*args):
+        pass
+
+    def get(*args):
+        return None
 
 class Actions(object):
     def __init__(self, config):
-        if 'plotdir' in config:
+        if 'plotdir' not in config:
+            self.plotq = DummyQueue()
+        else:
             logger.info('plots in {0} will be updated automatically'.format(config['plotdir']))
             if 'foremen logs' in config:
                 logger.info('foremen logs will be included from: {0}'.format(', '.join(config['foremen logs'])))
             plotter = Plotter(config['filename'], config['plotdir'])
-        else:
-            plotter = DummyPlotter()
 
-        def plotf(q):
-            while q.get() not in ('stop', None):
-                plotter.make_plots(foremen=config.get('foremen logs'))
+            def plotf(q):
+                while q.get() not in ('stop', None):
+                    plotter.make_plots(foremen=config.get('foremen logs'))
 
-        self.plotq = multiprocessing.Queue()
-        self.plotp = multiprocessing.Process(target=plotf, args=(self.plotq,))
-        self.plotp.start()
-        logger.info('spawning process for automatic plotting with pid {0}'.format(self.plotp.pid))
+            self.plotq = multiprocessing.Queue()
+            self.plotp = multiprocessing.Process(target=plotf, args=(self.plotq,))
+            self.plotp.start()
+            logger.info('spawning process for automatic plotting with pid {0}'.format(self.plotp.pid))
 
         self.__last = datetime.datetime.now()
 
@@ -38,3 +44,4 @@ class Actions(object):
         if (now - self.__last).seconds > 15 * 60:
             self.plotq.put('plot')
             self.__last = now
+

da73bd5235bea1030c8ed52ac270407a8be5913b
Matthias Wolf <matthias@sushinara.net>
2015-10-08 17:06:25 -0400

    Add missing file.

---
+++ lobster/status.py
@@ -0,0 +1,39 @@
+import logging
+import os
+from lobster import cmssw, util
+
+def status(args):
+    config = args.config
+    logger = logging.getLogger('lobster.status')
+    store = cmssw.jobit.JobitStore(config)
+
+    data = store.dataset_status()
+
+    widths = \
+            [max(map(len, (xs[0] for xs in data)))] + \
+            [max(map(len, (str(xs[i]) for xs in data))) for i in range(1, len(data[0]))]
+    data.insert(1, ['=' * w for w in widths])
+    headfmt = ' '.join('{{:^{0}}}'.format(w) for w in widths)
+    mainfmt = '{{:{0}}} '.format(widths[0]) + ' '.join('{{:>{0}}}'.format(w) for w in widths[1:])
+    report = \
+            headfmt.format(*data[0]) + '\n' + \
+            '\n'.join([mainfmt.format(*map(str, row)) for row in data[1:]])
+
+    logger.info("workflow summary:\n" + report)
+
+    wdir = config['workdir']
+    for cfg in config['tasks']:
+        label = cfg['label']
+        tasks = store.failed_jobits(label)
+        files = store.skipped_files(label)
+
+        if len(tasks) > 0:
+            msg = "tasks with failed jobits for {0}:".format(label)
+            for task in tasks:
+                tdir = os.path.normpath(os.path.join(wdir, label, 'failed', util.id2dir(task)))
+                msg += "\n" + tdir
+            logger.info(msg)
+
+        if len(files) > 0:
+            msg = "files skipped for {0}:\n".format(label) + "\n".join(files)
+            logger.info(msg)

ade15d8429a12a731388a52820019a67006db35b
Anna Elizabeth Woodard <awoodard@nd.edu>
2016-08-08 12:07:27 -0400

    Plugin `ProductionTaskHandler` for `EmptyDataset`s
    I hope these semantics aren't too confusing. But the functionality
    of `ProductionTaskHandler` is exactly what we need.

--- lobster/core/workflow.py
+++ lobster/core/workflow.py
@@ -7,7 +7,7 @@ import shutil
 import sys
 
 from lobster import fs, util
-from lobster.core.dataset import ParentDataset, ProductionDataset
+from lobster.core.dataset import ParentDataset, ProductionDataset, EmptyDataset
 from lobster.core.task import MergeTaskHandler, ProductionTaskHandler, TaskHandler
 from lobster.util import Configurable
 
@@ -376,7 +376,7 @@ class Workflow(Configurable):
     def handler(self, id_, files, lumis, taskdir, merge=False):
         if merge:
             return MergeTaskHandler(id_, self.label, files, lumis, list(self.get_outputs(id_)), taskdir)
-        elif isinstance(self.dataset, ProductionDataset):
+        elif isinstance(self.dataset, ProductionDataset) or isinstance(self.dataset, EmptyDataset):
             return ProductionTaskHandler(id_, self.label, lumis, list(self.get_outputs(id_)), taskdir)
         else:
             return TaskHandler(id_, self.label, files, lumis, list(self.get_outputs(id_)), taskdir, local=self.local)

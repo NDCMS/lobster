5eab8e29bdaa7d6b6b1778c2e01e9b50d4ad371e
Anna Elizabeth Woodard <awoodard@nd.edu>
2016-09-01 11:05:14 -0400

    Add more datasets to `simple.py`

--- examples/simple.py
+++ examples/simple.py
@@ -3,6 +3,8 @@ import datetime
 from lobster import cmssw
 from lobster.core import AdvancedOptions, Category, Config, StorageConfiguration, Workflow
 
+from lobster import datasets
+
 version = datetime.datetime.now().strftime('%Y%m%d_%H%M')
 
 storage = StorageConfiguration(
@@ -27,20 +29,20 @@ processing = Category(
 
 workflows = []
 
-ttH = Workflow(
-    label='ttH',
-    dataset=cmssw.Dataset(
-        dataset='/ttHToNonbb_M125_13TeV_powheg_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM',
-        events_per_task=5000
-    ),
-    category=processing,
-    pset='slim.py',
-    publish_label='test',
-    merge_size='3.5G',
-    outputs=['output.root']
-)
 
-workflows.append(ttH)
+for path in datasets('all'):
+    workflows.append(Workflow(
+        label='ttH',
+        dataset=cmssw.Dataset(
+            dataset=path,
+            events_per_task=5000
+        ),
+        category=processing,
+        pset='slim.py',
+        publish_label='test',
+        merge_size='3.5G',
+        outputs=['output.root']
+    )
 
 config = Config(
     workdir='/tmpscratch/users/$USER/lobster_test_' + version,
---
+++ lobster/datasets.py
@@ -0,0 +1,94 @@
+import re
+
+mctag = re.compile(r'RunIISpring16MiniAODv2-PUSpring16\w*_(reHLT_)?80X_mcRun2_asymptotic_(v14|2016_miniAODv2_v0).')
+
+datasets_ttH = """
+/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext1-v1/MINIAODSIM
+/ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/MINIAODSIM
+/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM
+/ttHToNonbb_M125_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM
+/ttHTobb_M125_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM
+/ttHToTT_M125_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM
+"""
+
+datasets_mc = """
+/WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/WpWpJJ_EWK-QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/WW_DoubleScattering_13TeV-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/tZq_ll_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTTT_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/MINIAODSIM
+/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/MINIAODSIM
+/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/MINIAODSIM
+/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v4/MINIAODSIM
+/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/MINIAODSIM
+/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/MINIAODSIM
+/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v2/MINIAODSIM
+/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/WWTo2L2Nu_13TeV-powheg/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+/ZZTo4L_13TeV_powheg_pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+"""
+
+datasets_collisions = """
+/SingleElectron/Run2016B-PromptReco-v2/MINIAOD
+/SingleElectron/Run2016C-PromptReco-v2/MINIAOD
+/SingleElectron/Run2016D-PromptReco-v2/MINIAOD
+/SingleElectron/Run2016E-PromptReco-v2/MINIAOD
+# /SingleElectron/Run2016F-PromptReco-v1/MINIAOD
+# /SingleElectron/Run2016G-PromptReco-v1/MINIAOD
+/SingleMuon/Run2016B-PromptReco-v2/MINIAOD
+/SingleMuon/Run2016C-PromptReco-v2/MINIAOD
+/SingleMuon/Run2016D-PromptReco-v2/MINIAOD
+/SingleMuon/Run2016E-PromptReco-v2/MINIAOD
+# /SingleMuon/Run2016F-PromptReco-v1/MINIAOD
+# /SingleMuon/Run2016G-PromptReco-v1/MINIAOD
+/DoubleEG/Run2016B-PromptReco-v2/MINIAOD
+/DoubleEG/Run2016C-PromptReco-v2/MINIAOD
+/DoubleEG/Run2016D-PromptReco-v2/MINIAOD
+/DoubleEG/Run2016E-PromptReco-v2/MINIAOD
+# /DoubleEG/Run2016F-PromptReco-v1/MINIAOD
+# /DoubleEG/Run2016G-PromptReco-v1/MINIAOD
+/DoubleMuon/Run2016B-PromptReco-v2/MINIAOD
+/DoubleMuon/Run2016C-PromptReco-v2/MINIAOD
+/DoubleMuon/Run2016D-PromptReco-v2/MINIAOD
+/DoubleMuon/Run2016E-PromptReco-v2/MINIAOD
+# /DoubleMuon/Run2016F-PromptReco-v1/MINIAOD
+# /DoubleMuon/Run2016G-PromptReco-v1/MINIAOD
+/MuonEG/Run2016B-PromptReco-v2/MINIAOD
+/MuonEG/Run2016C-PromptReco-v2/MINIAOD
+/MuonEG/Run2016D-PromptReco-v2/MINIAOD
+/MuonEG/Run2016E-PromptReco-v2/MINIAOD
+# /MuonEG/Run2016F-PromptReco-v1/MINIAOD
+# /MuonEG/Run2016G-PromptReco-v1/MINIAOD
+"""
+
+
+def datasets(tag):
+    if tag == 'all':
+        dsets = datasets_ttH + datasets_mc + datasets_collisions
+    elif tag == 'ttH':
+        dsets = datasets_ttH
+    elif tag == 'mc':
+        dsets = datasets_ttH + datasets_mc
+    elif tag == 'data':
+        dsets = datasets_collisions
+    elif tag == 'bkgs':
+        dsets = datasets_mc
+    else:
+        raise ValueError("invalid tag '{}'".format(tag))
+    return [ds.strip() for ds in dsets.split('\n') if len(ds) > 0 and not ds.startswith('#')]

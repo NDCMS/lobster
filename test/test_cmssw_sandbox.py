7204ae22eb8beac4cb8094c74cea916824f15e08
Matthias Wolf <matthias@sushinara.net>
2016-12-29 12:50:26 -0500

    Fix sandbox tests.

--- test/test_cmssw_sandbox.py
+++ test/test_cmssw_sandbox.py
@@ -18,16 +18,18 @@ class TestSandbox(unittest.TestCase):
     def test_localrt(self):
         os.environ['LOCALRT'] = 'data/sandbox/CMSSW_1_2_3'
         sandbox = lobster.cmssw.sandbox.Sandbox()
-        version, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
-        assert version == 'CMSSW_1_2_3'
+        version, arch, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
+        assert version == 'CMSSW_2_3_4'
+        assert arch == 'slc1_234'
 
     def test_version(self):
         sandbox = lobster.cmssw.sandbox.Sandbox(release='data/sandbox/CMSSW_1_2_3')
-        version, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
-        assert version == 'CMSSW_1_2_3'
+        version, arch, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
+        assert version == 'CMSSW_2_3_4'
+        assert arch == 'slc1_234'
 
     def test_include(self):
         sandbox = lobster.cmssw.sandbox.Sandbox(release='data/sandbox/CMSSW_1_2_3', include=['Foo/mydir'])
-        version, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
+        version, arch, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
         files = [f.name for f in tarfile.open(box)]
-        assert 'CMSSW_1_2_3/src/Foo/mydir' in files
+        assert 'CMSSW_2_3_4/src/Foo/mydir' in files

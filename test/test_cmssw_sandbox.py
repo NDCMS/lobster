9ce0b891878cc12a790a91d951561fe5abaa9a55
Matthias Wolf <matthias@sushinara.net>
2017-02-10 04:21:57 -0500

    Add test of sandbox recycling.

--- test/test_cmssw_sandbox.py
+++ test/test_cmssw_sandbox.py
@@ -33,3 +33,18 @@ class TestSandbox(unittest.TestCase):
         version, arch, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
         files = [f.name for f in tarfile.open(box)]
         assert 'CMSSW_2_3_4/src/Foo/mydir' in files
+
+    def test_recycle(self):
+        sandbox = lobster.cmssw.sandbox.Sandbox(release='data/sandbox/CMSSW_1_2_3', include=['Foo/mydir'])
+        version, arch, box = sandbox.package([os.path.dirname(__file__)], self.workdir)
+
+        tmpdir = os.path.join(self.workdir, 'tmpbox')
+        os.makedirs(tmpdir)
+        shutil.move(box, tmpdir)
+        box = os.path.join(tmpdir, os.path.basename(box))
+
+        sandbox2 = lobster.cmssw.sandbox.Sandbox(recycle=box)
+        version2, arch2, box2 = sandbox2.package([os.path.dirname(__file__)], self.workdir)
+
+        assert version2 == version
+        assert arch2 == arch

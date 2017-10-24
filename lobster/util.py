4f6fc4bbe7ec929cf9cdb59f5bf63a1f98b991f0
Matthias Wolf <matthias@sushinara.net>
2017-10-24 08:15:56 -0400

    Fix install from website without crash.
    Fixes #606.

--- lobster/util.py
+++ lobster/util.py
@@ -405,11 +405,15 @@ def get_version():
     else:
         start = os.getcwd()
         os.chdir(os.path.dirname(__file__))
-        head = subprocess.check_output(shlex.split('git rev-parse --short HEAD')).strip()
-        diff = subprocess.check_output(shlex.split('git diff'))
-        status = 'dirty' if diff else 'clean'
-        os.chdir(start)
-        version = '{major}-{head}-{status}'.format(major=VERSION, head=head, status=status)
+        try:
+            head = subprocess.check_output(shlex.split('git rev-parse --short HEAD')).strip()
+            diff = subprocess.check_output(shlex.split('git diff'))
+            status = 'dirty' if diff else 'clean'
+            version = '{major}-{head}-{status}'.format(major=VERSION, head=head, status=status)
+        except subprocess.CalledProcessError:
+            version = VERSION
+        finally:
+            os.chdir(start)
     return version
 
 

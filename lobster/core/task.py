NDCMS/lobster

commit b71e8aa7112c3ba72e1135d7420375d5c615f82f

Allow adjustable tasksizes for prod workflows
Before, users could specify `events_per_task`, `events_per_lumi`
(optionally) and `number_of_tasks`. The tasksize adjusts to meet the
requested runtime, but has no effect on the number of events processed
per task. This changes the dataset specification to `total_events` (what
the user ultimately cares about), `events_per_lumi`, and
`lumis_per_task` and adjusts `events_per_task` according to the
tasksize. In this way, adjustable tasksizes have the same effect as for
non-prod workflows. Fixes #609.

lobster/core/task.py
@@ -289,6 +289,7 @@ class ProductionTaskHandler(TaskHandler):
     def adjust(self, parameters, inputs, outputs, se):
         super(ProductionTaskHandler, self).adjust(parameters, inputs, outputs, se)
         parameters['mask']['first lumi'] = self._units[0][3]
+        parameters['mask']['events'] = parameters['mask']['events per lumi'] * len(self._units)
 
     def get_unit_info(self, failed, task_update, files_info, files_skipped, events_written):
         _, up = super(ProductionTaskHandler, self).get_unit_info(failed, task_update, files_info, files_skipped, events_written)

examples/mc_gen.py
@@ -25,9 +25,9 @@ lhe = Workflow(
     sandbox=cmssw.Sandbox(release='mc_gen/CMSSW_7_1_16_patch1'),
     merge_size='125M',
     dataset=ProductionDataset(
-        events_per_task=250,
+        total_events=25000,
         events_per_lumi=25,
-        number_of_tasks=100
+        lumis_per_task=10
     ),
     category=Category(
         name='lhe',

lobster/core/dataset.py
@@ -129,27 +129,25 @@ class ProductionDataset(Configurable):
 
     Parameters
     ----------
-        events_per_task : int
-            How many events to generate in one task.
+        total_events : int
+            How many events to generate.
         events_per_lumi : int
             How many events to generate in one luminosity section.
-        number_of_tasks : int
-            How many tasks to run.
+        lumis_per_task : int
+            How many lumis to produce per task. Can be
+            changed by Lobster to match the user-specified task runtime.
         randomize_seeds : bool
             Use random seeds every time a task is run.
     """
     _mutable = {}
 
-    def __init__(self, events_per_task, events_per_lumi=None, number_of_tasks=1, randomize_seeds=True):
-        self.number_of_tasks = number_of_tasks
-        self.events_per_task = events_per_task
+    def __init__(self, total_events, events_per_lumi=500, lumis_per_task=1, randomize_seeds=True):
+        self.total_events = total_events
         self.events_per_lumi = events_per_lumi
+        self.lumis_per_task = lumis_per_task
         self.randomize_seeds = randomize_seeds
 
-        nlumis = 1
-        if events_per_lumi:
-            nlumis = int(math.ceil(float(events_per_task) / events_per_lumi))
-        self.total_units = number_of_tasks * nlumis
+        self.total_units = int(math.ceil(float(total_events) / events_per_lumi))
 
     def validate(self):
         return True
@@ -158,13 +156,9 @@ class ProductionDataset(Configurable):
         dset = DatasetInfo()
         dset.file_based = True
 
-        ntasks = self.number_of_tasks
-        nlumis = 1
-        if self.events_per_lumi:
-            nlumis = int(math.ceil(float(self.events_per_task) / self.events_per_lumi))
-        dset.files[None].lumis = [(1, x) for x in range(1, ntasks * nlumis + 1, nlumis)]
-        dset.total_units = ntasks
-        self.total_units = ntasks * nlumis
+        dset.files[None].lumis = [(1, x) for x in range(1, self.total_units + 1)]
+        dset.total_units = self.total_units
+        dset.tasksize = self.lumis_per_task
 
         return dset
 
@@ -182,22 +176,25 @@ class MultiProductionDataset(ProductionDataset):
             pointing to a single gridpack file or directory.
         events_per_gridpack : int
             How many events to generate per gridpack.
-        events_per_task : int
-            How many events to generate in one task.
         events_per_lumi : int
             How many events to generate in one luminosity section.
+        lumis_per_task : int
+            How many lumis to produce per task. Can be
+            changed by Lobster to match the user-specified task runtime.
         randomize_seeds : bool
             Use random seeds every time a task is run.
     """
     _mutable = {}
 
-    def __init__(self, gridpacks, events_per_gridpack, events_per_task, events_per_lumi=None, randomize_seeds=True):
+    def __init__(self, gridpacks, events_per_gridpack, events_per_lumi=500, lumis_per_task=1, randomize_seeds=True):
         self.gridpacks = gridpacks
         self.events_per_gridpack = events_per_gridpack
-        self.events_per_task = events_per_task
         self.events_per_lumi = events_per_lumi
+        self.lumis_per_task = lumis_per_task
         self.randomize_seeds = randomize_seeds
-        self.total_units = 0
+
+        self.lumis_per_gridpack = int(math.ceil(float(events_per_gridpack) / events_per_lumi))
+        self.total_units = len(flatten(self.gridpacks)) * self.lumis_per_gridpack
 
     def validate(self):
         return len(flatten(self.gridpacks)) > 0
@@ -206,17 +203,12 @@ class MultiProductionDataset(ProductionDataset):
         dset = DatasetInfo()
         dset.file_based = True
 
-        lumis_per_task = 1
-        lumis_per_gridpack = int(math.ceil(float(self.events_per_gridpack) / self.events_per_task))
-        if self.events_per_lumi:
-            lumis_per_task = int(math.ceil(float(self.events_per_task) / self.events_per_lumi))
-            lumis_per_gridpack = int(math.ceil(float(self.events_per_gridpack) / self.events_per_lumi))
-
         for run, fn in enumerate(flatten(self.gridpacks)):
-            dset.files[fn].lumis = [(run, x) for x in range(1, lumis_per_gridpack + 1, lumis_per_task)]
+            dset.files[fn].lumis = [(run, x) for x in range(1, self.lumis_per_gridpack + 1)]
 
-        dset.total_units = len(sum([x.lumis for x in dset.files.values()], []))
-        self.total_units = dset.total_units
+        dset.total_units = self.total_units
+        dset.tasksize = self.lumis_per_task
+        dset.stop_on_file_boundary = True  # CMSSW can only handle one gridpack at a time
 
         return dset
 

lobster/core/workflow.py
@@ -472,7 +472,6 @@ class Workflow(Configurable):
         params['executable'] = cmd
         params['arguments'] = args
         if isinstance(self.dataset, ProductionDataset) and not merge:
-            params['mask']['events'] = self.dataset.events_per_task
             params['mask']['events per lumi'] = self.dataset.events_per_lumi
             params['randomize seeds'] = self.dataset.randomize_seeds
         else:

lobster/util.py
@@ -23,7 +23,7 @@ from contextlib import contextmanager
 from email.mime.text import MIMEText
 from pkg_resources import get_distribution
 
-VERSION = "1.6"
+VERSION = "1.7"
 
 logger = logging.getLogger('lobster.util')
 

commit d83f15451d849b016c10e63710d93699597be109

Fix bug causing crash with `EmptyDataset`
This bug was introduced by me in b71e8aa. Only `ProductionDataset`s get
'events per lumi' modified in `workflow.adjust`, but `ProductionDataset`
and `EmptyDataset` share a taskhandler which expects it to be there.

lobster/core/task.py
@@ -289,7 +289,8 @@ class ProductionTaskHandler(TaskHandler):
     def adjust(self, parameters, inputs, outputs, se):
         super(ProductionTaskHandler, self).adjust(parameters, inputs, outputs, se)
         parameters['mask']['first lumi'] = self._units[0][3]
-        parameters['mask']['events'] = parameters['mask']['events per lumi'] * len(self._units)
+        if 'events per lumi' in parameters['mask']:
+            parameters['mask']['events'] = parameters['mask']['events per lumi'] * len(self._units)
 
     def get_unit_info(self, failed, task_update, files_info, files_skipped, events_written):
         _, up = super(ProductionTaskHandler, self).get_unit_info(failed, task_update, files_info, files_skipped, events_written)